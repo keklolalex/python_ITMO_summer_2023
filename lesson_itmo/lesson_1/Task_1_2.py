e317d13782355cee1a1a08b9267c98410c36d160
aleksejmatankin <keklolpuck@gmail.com>
2023-05-03 01:12:49 +0300

    решена 2 задача

--- lesson_itmo/lesson_1/Task_1_2.py
+++ lesson_itmo/lesson_1/Task_1_2.py
@@ -1,17 +1,16 @@
 x = int(input())
 y = int(input())
-sum = x + y
-min = x - y
-prod = x * y
-delenie = int(x / y)
-ost = int(x // y)
-if (sum > min and sum > prod and sum > delenie and sum > ost):
-    print(sum)
-elif (min > sum and min > prod and  min > delenie and min > ost):
-    print(min)
-elif (prod > sum and prod > min and prod > delenie and prod > ost):
-    print(prod)
-elif (delenie > sum and delenie > min and delenie > prod and delenie > ost):
-    print(delenie)
-else:
-    print(ost)
+a = x + y
+b = x - y
+c = x * y
+d = int(x / y)
+e = int(x // y)
+w = a
+if (b > w): w = b
+if (c > w): w = c
+if (d > w): w = d
+if (e > w): w = e
+print(w)
+
+
+
